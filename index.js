fd4ea9d90637063d10562343421e49a9f0682630
unknown <rajebul.alam17@gmail.com>
2022-04-30 00:01:03 +0600

    Project User Name & Password Secured

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middle Ware
 app.use(cors());
 app.use(express.json());
+
+// Mongo DB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uzijp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// Main Function
+async function run() {
+    await client.connect();
+    const userCollection = client.db("Shop").collection("Product");
+
+    try {
+
+    } finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
 
 // Server Home
 app.get('/', async (req, res) => {
@@ -16,3 +37,5 @@ app.get('/', async (req, res) => {
 app.listen(port, () => {
     console.log('Server is running', port);
 })
+// Database User : rajebul-alam-khokan
+// database Pass : p5bmdPNRKV4ZQRXT
